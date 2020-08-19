93c27c367a13fb7594455485135bd637b8635f6a
Marisa Doig <mcr.doig@gmail.com>
2020-08-19 20:20:37 +0000

    Insert initial data for database

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -45,6 +45,6 @@ CREATE TABLE property_reviews (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  rating SMALLINT  NOT NULL DEFAULT 0,
+  rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
