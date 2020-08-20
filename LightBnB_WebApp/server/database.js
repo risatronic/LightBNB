5516d37e020e2af0145776b61a7f3f04f2946db3
Marisa Doig <mcr.doig@gmail.com>
2020-08-20 18:59:44 +0000

    Implement getAllReservations function querying database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -69,7 +69,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  
+  return pool.query(`
+  SELECT * FROM reservations
+  JOIN properties ON properties.id = property_id
+  WHERE guest_id = $1
+  LIMIT $2
+  `, values)
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
